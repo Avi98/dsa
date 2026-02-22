28f3870060c29197fe9e9d40b0b9cc98ae26b059
Avi98 <avinash.kumar.cs92@gmail.com>
2026-02-22 22:00:55 +0530

    binary search 1

---
+++ binary_search/peak_element_in_array.py
@@ -0,0 +1,35 @@
+"""
+Problem Statement: Given an array of length N, peak element is defined as the element greater than both of its neighbors. Formally, if arr[i] is the peak element, arr[i - 1] < arr[i] and arr[i + 1] < arr[i]. Find the index(0-based) of a peak element in the array.
+If there are multiple peak numbers, return the index of any peak number.
+
+Input: arr[] = {1,2,3,4,5,6,7,8,5,1}
+Output: 7
+Explanation: There is only 1 peak element, 8,  that is at index 7.
+Input: arr[] = {1,2,1,3,5,6,4}
+Output: 1
+Explanation : There are 2 peak numbers that are at indices 1 and 5. We can return any of them.
+"""
+
+import math
+
+
+def peak_element_in_array(arr: list[int]) -> int:
+    low = 0
+    high = len(arr) - 1
+
+    while low < high:
+        mid = math.ceil((low + high) / 2)
+
+        if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+            return mid
+
+        if arr[mid] < arr[mid + 1]:
+            low = mid
+        else:
+            high = mid
+    return -1
+
+
+if __name__ == "__main__":
+    ans = peak_element_in_array([1, 2, 1, 3, 5, 6, 4])
+    print(ans)
